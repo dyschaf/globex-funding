5f3ee30f1a5385090689d54862006547700bfacb
dyschaf <schaffeldovid@gmail.com>
2023-03-01 18:30:08 +0200

    add footer js

--- client-side/src/component/Footer.js
+++ client-side/src/component/Footer.js
@@ -1,7 +1,20 @@
 function Footer(){
     return(
         <>
-            
+            <div>
+                <img src="" alt=""></img>
+            </div>
+            <div>
+                <h2>SITE</h2>
+                <h6>Home</h6>
+                <h6>Apply</h6>
+                <h6>ISO Partnership</h6>
+            </div>
+            <div>
+                <h2>Contact Info</h2>
+                <h6>Number</h6>
+                <h6>Email</h6>
+            </div>
         </>
         
     )
