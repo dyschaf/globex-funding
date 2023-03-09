5a99dae526a90204cc956ff2b6abcd7a0fc63ba9
dschaf <schaffeldovid@gmail.com>
2023-03-09 20:06:05 +0200

    add to footer.js

--- client-side/src/component/Footer.js
+++ client-side/src/component/Footer.js
@@ -5,13 +5,22 @@ function Footer(){
                 <img src="" alt=""></img>
             </div>
             <div>
+                
                 <h2>SITE</h2>
-                <h6>Home</h6>
-                <h6>Apply</h6>
-                <h6>ISO Partnership</h6>
+                <a href="">
+                    <h6>Home</h6>
+                </a>
+                <a href="/form">
+                    <h6>Apply</h6>
+                </a>
+                <a href="">
+                    <h6>ISO Partnership</h6>
+                </a>
             </div>
             <div className="footerRight">
+                
                 <h2>Contact Info</h2>
+                
                 <h6>Number</h6>
                 <h6>Email</h6>
             </div>
